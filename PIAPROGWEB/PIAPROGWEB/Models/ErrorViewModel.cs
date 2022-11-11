728105e2f3c7fa8f55a4b604398c904afed48883
juanfra109 <114134747+juanfra109@users.noreply.github.com>
2022-11-10 18:26:15 -0600

    creación de proyecto

---
+++ PIAPROGWEB/PIAPROGWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PIAPROGWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
